2ada0c5732d2ca80d8adf4b02b6a1d2ab0255bd4
Leandro Reis <lleandrofr@gmail.com>
2021-05-13 11:33:59 -0300

    fix sfx bug for the main buttons

---
+++ src/components/MainButtons.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Howl, Howler } from 'howler';
+import start from '../sounds/start.mp3';
+import pause from '../sounds/pause.mp3';
+import stop from '../sounds/impostor-win.mp3';
+
+const sfx = [
+  { label: 'start', sound: start },
+  { label: 'pause', sound: pause },
+  { label: 'stop', sound: stop },
+];
+
+class MainButtons extends Component {
+  constructor() {
+    super();
+    this.soundPlay = this.soundPlay.bind(this);
+    this.renderButtons = this.renderButtons.bind(this);
+  }
+
+  soundPlay(src) {
+    const sound = new Howl({
+      src,
+      volume: 0.5,
+    });
+    sound.play();
+  }
+
+  renderButtons() {
+    return sfx.map((soundObj, index) => (
+      <button
+        type="button"
+        key={ index }
+        id={ `${soundObj.label}-btn` }
+        onClick={ () => this.soundPlay(soundObj.sound) }
+      >
+        {soundObj.label}
+      </button>
+    ));
+  }
+
+  render() {
+    const VOLUME = 0.1;
+    Howler.volume(VOLUME);
+    return (
+      <div className="main-buttons">
+        {this.renderButtons()}
+      </div>);
+  }
+}
+
+export default MainButtons;
