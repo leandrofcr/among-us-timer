2164738bf5a4f8d881c20d1a67e0f09ae034fa75
Leandro Reis <lleandrofr@gmail.com>
2021-05-12 22:41:31 -0300

    positions the elements in the container

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import DialPad from './components/DialPad';
 import Header from './components/Header';
-import timer from './images/timer.png';
+// import timer from './images/timer.png';
+// <img src={ timer } alt="timer background" className="timer-background" />
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <div className="timer-container">
-          <img src={ timer } alt="timer background" className="timer-background" />
-          <div className="timer">Timer</div>
+        <div className="main-container">
+          <div className="timer-container">
+            <span className="timer">
+              05:30
+            </span>
+          </div>
+          <div className="dialPad-container">
+            <DialPad />
+          </div>
 
-          <DialPad />
         </div>
 
       </div>
