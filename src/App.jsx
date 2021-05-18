408254652127ab4dbfbd64f61a4ae3b50ce630c8
Leandro Reis <lleandrofr@gmail.com>
2021-05-17 23:50:17 -0300

    creates a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import MainButtons from './components/MainButtons';
 import Stars from './components/Stars';
@@ -129,6 +130,7 @@ class App extends Component {
           pauseTimer={ this.pauseTimer }
           stopTimer={ this.stopTimer }
         />}
+        {!showAnimation && <Footer />}
       </div>
     );
   }
